4c7454941e3d41aa3b0ae7f1dbfd1e2b3d5bf2b9
yusufabol <yusufabolarinwa@gmail.com>
2025-10-01 16:09:39 +0100

    updated stage_04 and main

---
+++ projects/covid_chest_classifier/src/ccclassifier/pipeline/stage_04_model_evaluation.py
@@ -0,0 +1,18 @@
+from ccclassifier.config.configuration import ConfigurationManager
+from ccclassifier.components.model_evaluation_mlflow import Evaluation
+from ccclassifier import logger
+
+
+STAGE_NAME = "Evaluation stage"
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        eval_config = config.get_evaluation_config()
+        evaluation = Evaluation(eval_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+        evaluation.log_into_mlflow()
